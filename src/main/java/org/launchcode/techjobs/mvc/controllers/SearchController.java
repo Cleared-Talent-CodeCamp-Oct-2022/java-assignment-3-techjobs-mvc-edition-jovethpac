427c5ab9674808e0fd6f8a8be7b15449ee14dc63
Joveth Sampson <jovethpac@yahoo.com>
2023-02-15 12:53:26 -0500

    Second push to git

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -1,9 +1,15 @@
 package org.launchcode.techjobs.mvc.controllers;
 
+import org.launchcode.techjobs.mvc.models.Job;
+import org.launchcode.techjobs.mvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
 
 import static org.launchcode.techjobs.mvc.controllers.ListController.columnChoices;
 
@@ -22,5 +28,26 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
+
+    @PostMapping("results")
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm){
+        ArrayList<Job> jobs;
+
+        if (searchType.equals("all") || searchType.equals("")) { //if all is selected
+            jobs = JobData.findAll();//print all
+        } else {//otherwise if it's besides all, send this and pass in their choices, pass in parameters, change column, value??
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+        model.addAttribute("jobs", jobs); //first part is string, second is any variable I make up
+        model.addAttribute("columns", columnChoices);//shows the radio buttons on the webpage
+        return "search";
+    }
+
+
+
+
+
+
+
 
 }
